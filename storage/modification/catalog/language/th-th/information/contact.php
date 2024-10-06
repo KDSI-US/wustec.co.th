7e1f71681485c52962cab96e611c0cca0fc8991d
Richard <rickyrich0988@gmail.com>
2024-10-05 22:27:45 -0700

    Add thai for contact us page

--- storage/modification/catalog/language/th-th/information/contact.php
+++ storage/modification/catalog/language/th-th/information/contact.php
@@ -1,28 +1,31 @@
 <?php
 /* This file is under Git Control by KDSI. */
 // Heading
-$_['heading_title']  = 'Contact Us';
+$_['heading_title']  = 'ติดต่อเรา';
 
 // Text
 $_['text_location']  = 'Our Location';
 $_['text_store']     = 'Our Stores';
-$_['text_contact']   = 'Contact Form';
-$_['text_address']   = 'Address';
-$_['text_telephone'] = 'Telephone';
-$_['text_fax']       = 'Fax';
-$_['text_open']      = 'Opening Times';
+$_['text_contact']   = 'ติดต่อ';
+$_['text_address']   = 'ที่อยู่';
+$_['text_telephone'] = 'โทรศัพท์';
+$_['text_fax']       = 'โทรสาร';
+$_['text_email']     = 'อีเมล';
 $_['text_comment']   = 'Comments';
-$_['text_message']   = '<p>Your enquiry has been successfully sent to the store owner!</p>'; 
+$_['text_touch']     = 'ได้รับการติดต่อ';
+$_['text_website']   = 'เว็บไซต์';
+$_['text_message']   = '<p>คำถามของคุณถูกส่งเรียบร้อยแล้ว!</p>'; 
 
 // Entry
-$_['entry_name']     = 'Your Name';
-$_['entry_email']    = 'E-Mail Address';
-$_['entry_enquiry']  = 'Enquiry';
+$_['entry_name']     = 'ชื่อของคุณ';
+$_['entry_email']    = 'อีเมล';
+$_['entry_enquiry']  = 'ความคิดเห็น';
 
 // Email
-$_['email_subject']  = 'Enquiry %s';
+$_['email_subject']  = 'ความคิดเห็น %s';
+$_['button_submit']  = 'ส่ง';
 
 // Errors
-$_['error_name']     = 'Name must be between 3 and 32 characters!';
-$_['error_email']    = 'E-Mail Address does not appear to be valid!';
-$_['error_enquiry']  = 'Enquiry must be between 10 and 3000 characters!';
+$_['error_name']     = 'ชื่อต้องมีความยาวระหว่าง 3 ถึง 32 อักขระ!';
+$_['error_email']    = 'ที่อยู่อีเมลดูเหมือนจะไม่ถูกต้อง!';
+$_['error_enquiry']  = 'ความคิดเห็นต้องมีความยาวระหว่าง 10 ถึง 3,000 ตัวอักษร!';
